0a3b54dee4728f3899dc20bfb81032e6b35db827
Mitchelle Makini <mitchellemakini15@gmail.com>
2019-04-30 02:37:10 +0300

    created index view

---
+++ app/main/views.py
@@ -0,0 +1,9 @@
+from flask import render_template,redirect, url_for
+from flask_login import current_user,login_required
+from . import main
+
+@main.route("/",methods=["GET","POST"])
+def index():
+    return render_template('index.html')
+
+
