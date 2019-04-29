51bec8ace0277ca30efebe2fa73464c8f270afbf
Mitchelle Makini <mitchellemakini15@gmail.com>
2019-04-30 02:39:03 +0300

    load all blogs from db

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,11 @@
 from flask import render_template,redirect, url_for
 from flask_login import current_user,login_required
 from . import main
+from app.models import Post
 
 @main.route("/",methods=["GET","POST"])
 def index():
-    return render_template('index.html')
+    blogs=Post.query.all()
+    return render_template('index.html',blogs=blogs)
 
 
