af7820841d9b0fe8e8f9ee896c428c04048306dd
Mitchelle Makini <mitchellemakini15@gmail.com>
2019-04-30 04:40:05 +0300

    implemented comment delete

--- app/main/views.py
+++ app/main/views.py
@@ -4,6 +4,7 @@ from . import main
 from .forms import BlogForm, CommentForm
 from app.models import Post,Comment
 from app.request_handle import get_quote
+from app import db
 
 @main.route("/",methods=["GET","POST"])
 def index():
@@ -39,3 +40,14 @@ def blog(blog_id):
     blog=Post.query.filter_by(id=blog_id).first()
     context={"blog":blog,"comments":comments,"comment_form":comment_form,"quote":get_quote()}
     return render_template("./main/blog.html",**context)
+
+
+@main.route("/comment/delete/<int:comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment=Comment.query.filter_by(id=comment_id).first()
+    blog_id=comment.post_id
+    db.session.delete(comment)
+    db.session.commit()
+    return redirect(url_for("main.blog",blog_id=blog_id))
+
