0ad03aff9bb3d70b37c1e4e00930975b499641d2
Mitchelle Makini <mitchellemakini15@gmail.com>
2019-04-30 03:16:06 +0300

    created view for blog & comment, created blog template

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template,redirect, url_for
 from flask_login import current_user,login_required
 from . import main
 from .forms import BlogForm, CommentForm
-from app.models import Post
+from app.models import Post,Comment
 from app.request_handle import get_quote
 
 @main.route("/",methods=["GET","POST"])
@@ -23,3 +23,19 @@ def add_blog():
         blog.save()
         return redirect(url_for("main.index"))
     return render_template('./main/add_blog.html',blog_form=blog_form,quote=get_quote())
+
+
+@main.route("/blog/<int:blog_id>",methods=["GET","POST"])
+def blog(blog_id):
+    comment_form=CommentForm()
+    if comment_form.validate_on_submit():
+        comment=Comment(
+            user_id=current_user.id,
+            post_id=blog_id,
+            text=comment_form.comment.data
+        )
+        comment.save()
+    comments=Comment.query.filter_by(post_id=blog_id)
+    blog=Post.query.filter_by(id=blog_id).first()
+    context={"blog":blog,"comments":comments,"comment_form":comment_form,"quote":get_quote()}
+    return render_template("./main/blog.html",**context)
